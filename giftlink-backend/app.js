4b3198d65d7ed565e535759909577b2bd5878a67
joaofaria-src <17826036+joaofaria-src@users.noreply.github.com>
2024-12-22 05:33:06 -0500

    router config

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -23,10 +22,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftroutes = require('./giftRoutes');
+const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./searchRoutes');
+//{{insert code here}}
 
 
 const pinoHttp = require('pino-http');
@@ -39,7 +38,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/gifts', searchRoutes);
+//{{insert code here}}
 
 
 // Global Error Handler
